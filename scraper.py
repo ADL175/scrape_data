5cddd8fbfc9d998d6e115ef5b0692447109df52b
ADL175 <armydavidlim@gmail.com>
2017-06-17 20:56:06 -0700

    completed scraper

--- scraper.py
+++ scraper.py
@@ -36,16 +36,16 @@ def get_inspection_page(**kwargs):
     return resp.content, resp.encoding
 
 
-def load_inspection_page(**kwargs):
+# def load_inspection_page(**kwargs):
     # url = inspection_page.html
-    import pdb; pdb.set_trace()
-    params = INSPECTION_PARAMS.copy()
-    for key, value in kwargs.items():
-        if key in INSPECTION_PARAMS:
-            params[key] = value
-    resp = requests.get(url, params=params)
-    resp.raise_for_status()
-    return resp.content, resp.encoding
+    # import pdb; pdb.set_trace()
+    # params = INSPECTION_PARAMS.copy()
+    # for key, value in kwargs.items():
+    #     if key in INSPECTION_PARAMS:
+    #         params[key] = value
+    # resp = requests.get(url, params=params)
+    # resp.raise_for_status()
+    # return resp.content, resp.encoding
 
 
 def extract_data_listings(html):
@@ -65,26 +65,90 @@ def has_two_tds(elem):
     return is_tr and has_two
 
 
+def clean_data(td):
+    data = td.string
+    try:
+        return data.strip(" \n:-")
+    except AttributeError:
+        return u""
+
+
+def extract_restaurant_metadata(elem):
+    metadata_rows = elem.find('tbody').find_all(
+        has_two_tds, recursive=False
+    )
+    rdata = {}
+    current_label = ''
+    for row in metadata_rows:
+        key_cell, val_cell = row.find_all('td', recursive=False)
+        new_label = clean_data(key_cell)
+        current_label = new_label if new_label else current_label
+        rdata.setdefault(current_label, []).append(clean_data(val_cell))
+    return rdata
+
+
+def is_inspection_row(elem):
+    is_tr = elem.name == 'tr'
+    if not is_tr:
+        return False
+    td_children = elem.find_all('td', recursive=False)
+    has_four = len(td_children) == 4
+    this_text = clean_data(td_children[0]).lower()
+    contains_word = 'inspection' in this_text
+    does_not_start = not this_text.startswith('inspection')
+    return is_tr and has_four and contains_word and does_not_start
+
+
+def extract_score_data(elem):
+    inspection_rows = elem.find_all(is_inspection_row)
+    samples = len(inspection_rows)
+    total = high_score = average = 0
+    for row in inspection_rows:
+        strval = clean_data(row.find_all('td')[2])
+        try:
+            intval = int(strval)
+        except (ValueError, TypeError):
+            samples -= 1
+        else:
+            total += intval
+            high_score = intval if intval > high_score else high_score
+    if samples:
+        average = total/float(samples)
+    data = {
+        u'Average Score': average,
+        u'High Score': high_score,
+        u'Total Inspections': samples
+    }
+    return data
+
 if __name__ == "__main__":
     kwargs = {
         'Inspection_Start': '2/1/2013',
         'Inspection_End': '2/1/2015',
         'Zip_Code': '98109'
     }
-    if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        html, encoding = load_inspection_page('inspection_page.html')
-    else:
-        html, encoding = get_inspection_page(**kwargs)
+    # if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    #     html, encoding = load_inspection_page('inspection_page.html')
+    # else:
+    html, encoding = get_inspection_page(**kwargs)
     doc = parse_source(html, encoding)
     listings = extract_data_listings(doc)
     for listing in listings[:5]:
-        metadata_rows = listing.find('tbody').find_all(
-            has_two_tds, recursive=False
-        )
-        for row in metadata_rows:
-            for td in row.find_all('td', recursive=False):
-                print(td.text)
-            print()
-        print()
+        metadata = extract_restaurant_metadata(listing)
+        score_data = extract_score_data(listing)
+        print(score_data)
+        # inspection_rows = listing.find_all(is_inspection_row)
+        # for row in inspection_rows:
+            # print(row.text)
+        # print(metadata)
+        # print()
+        # metadata_rows = listing.find('tbody').find_all(
+            # has_two_tds, recursive=False
+        # )
+        # for row in metadata_rows:
+        #     for td in row.find_all('td', recursive=False):
+        #         print(repr(clean_data(td)))
+        #     print()
+        # print()
         # print(len(metadata_rows))
     # print(doc.prettify(encoding=encoding))
